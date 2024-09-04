9cfbe2da982e548ebe0b71b81c1b408f92ef7c8c
Maria Eduarda Ferreira <maria.ferreira@feedz.com.br>
2024-09-04 20:27:35 +0900

    Create GET posts

---
+++ src/contexts/postsContext.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const PostsContext = createContext()
+
+export function PostsProvider({ children }) {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://www.googleapis.com/blogger/v3/blogs/${process.env.REACT_APP_blogId}/posts?key=${process.env.REACT_APP_APIKey}`
+                );
+                setPosts(response.data.items);
+            } catch (error) {
+                console.error('Erro ao buscar posts do Blogger:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <PostsContext.Provider value={{ posts }}>
+            {children}
+        </PostsContext.Provider>
+    )
+}
